e38af25cb6331b83ccd0c446eb66c8fde0b3863c
Thomas Foucault <t.foucault2@gmail.com>
2020-02-01 01:32:24 +0200

    cleean circle

--- Assets/Scripts/Circle.cs
+++ Assets/Scripts/Circle.cs
@@ -7,42 +7,44 @@ public class Circle : MonoBehaviour
     public Material CircleMat;
     public float AnimationDuration;
 
-
-    private Queue<float> LineWidth;
-    private Queue<float> Radius;
+    private Queue<float> _lineWidth;
+    private Queue<float> _radius;
+    private Color _color;
 
     private LineRenderer _line;
-    //private bool _updateCircle = true;
-    public bool _reduce = false;
+    private bool _reduce = false;
     private float _reduceCounter = 0;
     private float _currentRadius;
     private float _currentWidth;
 
     private static float _small = 0.000001f;
 
+    public Circle(Queue<float> lineWidth, Queue<float> radius, Color color)
+    {
+        _lineWidth = lineWidth;
+        _radius = radius;
+        _color = color;
+    }
+
     void Awake()
     {
-        LineWidth = new Queue<float>();
-        Radius = new Queue<float>();
-        LineWidth.Enqueue(0.5f);
-        LineWidth.Enqueue(0.5f);
-        LineWidth.Enqueue(0.25f);
-        Radius.Enqueue(3f);
-        Radius.Enqueue(1.5f);
-        Radius.Enqueue(0.75f);
+        _lineWidth = new Queue<float>();
+        _radius = new Queue<float>();
+        _lineWidth.Enqueue(0.5f);
+        _lineWidth.Enqueue(0.5f);
+        _lineWidth.Enqueue(0.25f);
+        _radius.Enqueue(3f);
+        _radius.Enqueue(1.5f);
+        _radius.Enqueue(0.75f);
+        _color = Color.yellow;
 
         _line = gameObject.AddComponent<LineRenderer>();
-        DrawCircle(Radius.Peek(), LineWidth.Peek());
+        DrawCircle(_radius.Peek(), _lineWidth.Peek());
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (_updateCircle)
-        //{
-        //    _updateCircle = false;
-        //    DrawCircle(Radius, LineWidth);
-        //}
         // Asked to reduce
         if (_reduce)
         {
@@ -52,35 +54,27 @@ public class Circle : MonoBehaviour
             {
                 _reduce = false;
                 _reduceCounter = 0;
-                if (LineWidth.Count == 0 && Radius.Count == 0)
+                if (_lineWidth.Count == 0 && _radius.Count == 0)
                 {
                     // trigger destroy level
                 }
             } else
             {
                 // reducing
-                var newWidth = Mathf.Lerp(_currentWidth, LineWidth.Count > 0 ? LineWidth.Peek() : _small, _reduceCounter);
-                var newRadius = Mathf.Lerp(_currentRadius, Radius.Count > 0 ? Radius.Peek() : _small, _reduceCounter);
+                var newWidth = Mathf.Lerp(_currentWidth, _lineWidth.Count > 0 ? _lineWidth.Peek() : _small, _reduceCounter);
+                var newRadius = Mathf.Lerp(_currentRadius, _radius.Count > 0 ? _radius.Peek() : _small, _reduceCounter);
                 DrawCircle(newRadius, newWidth);
             }
         }
     }
 
-    private void OnValidate()
-    {
-        // called when value in inspector is changed
-        //_updateCircle = true;
-    }
-
     public void Reduce()
     {
-        //_updateCircle = true;
         _reduce = true;
-        _currentRadius = Radius.Peek();
-        _currentWidth = LineWidth.Peek();
-        Debug.Log(_currentRadius + " " + _currentWidth);
-        Radius.Dequeue();
-        LineWidth.Dequeue();
+        _currentRadius = _radius.Peek();
+        _currentWidth = _lineWidth.Peek();
+        _radius.Dequeue();
+        _lineWidth.Dequeue();
     }
 
     private void DrawCircle(float radius, float lineWidth)
@@ -91,6 +85,7 @@ public class Circle : MonoBehaviour
         _line.endWidth = lineWidth;
         _line.positionCount = segments + 1;
         _line.material = CircleMat;
+        _line.material.color = _color;
 
         var pointCount = segments + 1; // add extra point to make startpoint and endpoint the same to close the circle
         var points = new Vector3[pointCount];
