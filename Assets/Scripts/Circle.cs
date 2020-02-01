b0f0bb76d0ca8b3aa2932cad9f44cd809bf5fa3d
Lionel Jouin <lionel.jouin@outlook.com>
2020-02-01 13:42:28 +0100

    Fix imports and namespaces

--- Assets/Scripts/Circle.cs
+++ Assets/Scripts/Circle.cs
@@ -1,149 +1,152 @@
-using System.Collections;
+using Assets.Scripts.Interfaces;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Circle : MonoBehaviour, ICircle
+namespace Assets.Scripts
 {
-
-    public event DestroyHandler OnDestroyFinished;
-
-    public Material CircleMat;
-    public Material ToConstructMat;
-    public float AnimationDuration;
-
-    private List<float[]> _toConstructDegrees;
-
-    private List<float> _lineWidth;
-    private List<float> _radius;
-    private Color _color;
-
-    private GameObject _circleBase;
-    private List<GameObject> _circlesToConstruct;
-    private List<GameObject> _destructedCircles;
-
-    private GameObject _destructionCircleInConstruction;
-    private bool _destroying;
-
-    public Circle(List<float> lineWidth, List<float> radius, Color color, List<float[]> toConstructDegrees)
+    public class Circle : MonoBehaviour, ICircle
     {
-        _lineWidth = lineWidth;
-        _radius = radius;
-        _color = color;
-        _toConstructDegrees = toConstructDegrees;
-    }
 
-    void Awake()
-    {
-        _lineWidth = new List<float>();
-        _radius = new List<float>();
-        _lineWidth.Add(0.75f);
-        _lineWidth.Add(0.5f);
-        _lineWidth.Add(0.25f);
-        _radius.Add(3f);
-        _radius.Add(1.5f);
-        _radius.Add(0.75f);
-        _color = Color.yellow;
+        public event DestroyHandler OnDestroyFinished;
 
-        _toConstructDegrees = new List<float[]>();
-        _toConstructDegrees.Add(new float[] { 20, 50 });
-        _toConstructDegrees.Add(new float[] { 120, 200 });
-        //////////////// above is temporary stuff
+        public Material CircleMat;
+        public Material ToConstructMat;
+        public float AnimationDuration;
 
-        _circlesToConstruct = new List<GameObject>();
-        _destructedCircles = new List<GameObject>();
-        _circleBase = new GameObject("Circle base");
-        _circleBase.transform.parent = transform;
+        private List<float[]> _toConstructDegrees;
 
+        private List<float> _lineWidth;
+        private List<float> _radius;
+        private Color _color;
 
-        var baseCC = _circleBase.AddComponent<CircleComponent>();
-        baseCC.Init(_lineWidth, _radius, _color, CircleMat, false, AnimationDuration, null);
+        private GameObject _circleBase;
+        private List<GameObject> _circlesToConstruct;
+        private List<GameObject> _destructedCircles;
 
-        foreach (var section in _toConstructDegrees)
+        private GameObject _destructionCircleInConstruction;
+        private bool _destroying;
+
+        public Circle(List<float> lineWidth, List<float> radius, Color color, List<float[]> toConstructDegrees)
         {
-            var ctc = new GameObject("Circle to construct");
-            ctc.transform.parent = transform;
-            ctc.transform.position = new Vector3(0, 0, -5);
-            var tcCC = ctc.AddComponent<CircleComponent>();
-            tcCC.Init(_lineWidth, _radius, _color, ToConstructMat, true, AnimationDuration, section);
-
-            _circlesToConstruct.Add(ctc);
+            _lineWidth = lineWidth;
+            _radius = radius;
+            _color = color;
+            _toConstructDegrees = toConstructDegrees;
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
-    public void Reduce()
-    {
-        _circleBase.GetComponent<CircleComponent>().Reduce();
-        foreach (var circle in _circlesToConstruct)
+        void Awake()
         {
-            circle.GetComponent<CircleComponent>().Reduce();
-        }
-    }
+            _lineWidth = new List<float>();
+            _radius = new List<float>();
+            _lineWidth.Add(0.75f);
+            _lineWidth.Add(0.5f);
+            _lineWidth.Add(0.25f);
+            _radius.Add(3f);
+            _radius.Add(1.5f);
+            _radius.Add(0.75f);
+            _color = Color.yellow;
 
-    public void UpdatePress(float degree)
-    {
-        var shouldDestroy = true;
-        bool lastPress = false;
-        // redraw the to construct circles with the updated angles
-        for (var i = 0; i < _circlesToConstruct.Count; i++)
-        {
-            var circle = _circlesToConstruct[i].GetComponent<CircleComponent>();
-            var degs = circle.GetDegrees();
-            if (degree > degs[0] && degree < degs[1])
+            _toConstructDegrees = new List<float[]>();
+            _toConstructDegrees.Add(new float[] { 20, 50 });
+            _toConstructDegrees.Add(new float[] { 120, 200 });
+            //////////////// above is temporary stuff
+
+            _circlesToConstruct = new List<GameObject>();
+            _destructedCircles = new List<GameObject>();
+            _circleBase = new GameObject("Circle base");
+            _circleBase.transform.parent = transform;
+
+
+            var baseCC = _circleBase.AddComponent<CircleComponent>();
+            baseCC.Init(_lineWidth, _radius, _color, CircleMat, false, AnimationDuration, null);
+
+            foreach (var section in _toConstructDegrees)
             {
-                // we tap in a to construct circle
-                var prevDegree = degs[0];
-                circle.UpdateDegreeIn(degree);
-                circle.RedrawCircle();
-                shouldDestroy = false;
+                var ctc = new GameObject("Circle to construct");
+                ctc.transform.parent = transform;
+                ctc.transform.position = new Vector3(0, 0, -5);
+                var tcCC = ctc.AddComponent<CircleComponent>();
+                tcCC.Init(_lineWidth, _radius, _color, ToConstructMat, true, AnimationDuration, section);
 
-                // if we were destroying just before, we need to stop
+                _circlesToConstruct.Add(ctc);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+
+        public void Reduce()
+        {
+            _circleBase.GetComponent<CircleComponent>().Reduce();
+            foreach (var circle in _circlesToConstruct)
+            {
+                circle.GetComponent<CircleComponent>().Reduce();
+            }
+        }
+
+        public void UpdatePress(float degree)
+        {
+            var shouldDestroy = true;
+            bool lastPress = false;
+            // redraw the to construct circles with the updated angles
+            for (var i = 0; i < _circlesToConstruct.Count; i++)
+            {
+                var circle = _circlesToConstruct[i].GetComponent<CircleComponent>();
+                var degs = circle.GetDegrees();
+                if (degree > degs[0] && degree < degs[1])
+                {
+                    // we tap in a to construct circle
+                    var prevDegree = degs[0];
+                    circle.UpdateDegreeIn(degree);
+                    circle.RedrawCircle();
+                    shouldDestroy = false;
+
+                    // if we were destroying just before, we need to stop
+                    if (_destroying)
+                    {
+                        circle = _destructionCircleInConstruction.GetComponent<CircleComponent>();
+                        circle.UpdateDegreeOut(prevDegree);
+                        circle.RedrawCircle();
+                        _destroying = false;
+                        _destructedCircles.Add(_destructionCircleInConstruction);
+                    }
+
+                    break;
+                }
+            }
+
+
+            // if it doesnt construct, it destroys on normal angles
+            // we need to create a new circle
+            if (shouldDestroy)
+            {
+                // we update the circle in destruction if we're already destroying it
                 if (_destroying)
                 {
-                    circle = _destructionCircleInConstruction.GetComponent<CircleComponent>();
-                    circle.UpdateDegreeOut(prevDegree);
+                    var circle = _destructionCircleInConstruction.GetComponent<CircleComponent>();
+                    circle.UpdateDegreeOut(degree);
                     circle.RedrawCircle();
-                    _destroying = false;
-                    _destructedCircles.Add(_destructionCircleInConstruction);
+                    if (lastPress)
+                    {
+                        _destroying = false;
+                        _destructedCircles.Add(_destructionCircleInConstruction);
+                    }
                 }
-
-                break;
-            }
-        }
-
-
-        // if it doesnt construct, it destroys on normal angles
-        // we need to create a new circle
-        if (shouldDestroy)
-        {
-            // we update the circle in destruction if we're already destroying it
-            if (_destroying)
-            {
-                var circle = _destructionCircleInConstruction.GetComponent<CircleComponent>();
-                circle.UpdateDegreeOut(degree);
-                circle.RedrawCircle();
-                if (lastPress)
+                else
                 {
-                    _destroying = false;
-                    _destructedCircles.Add(_destructionCircleInConstruction);
+                    // else we create a destruction circle
+                    _destroying = true;
+                    _destructionCircleInConstruction = new GameObject("Circle to construct");
+                    _destructionCircleInConstruction.transform.parent = transform;
+                    _destructionCircleInConstruction.transform.position = new Vector3(0, 0, -5);
+                    var tcCC = _destructionCircleInConstruction.AddComponent<CircleComponent>();
+                    tcCC.Init(_lineWidth, _radius, _color, ToConstructMat, true, AnimationDuration, new float[] { degree, degree });
+                    tcCC.StepsIndex = _circleBase.GetComponent<CircleComponent>().StepsIndex;
                 }
-            }
-            else
-            {
-                // else we create a destruction circle
-                _destroying = true;
-                _destructionCircleInConstruction = new GameObject("Circle to construct");
-                _destructionCircleInConstruction.transform.parent = transform;
-                _destructionCircleInConstruction.transform.position = new Vector3(0, 0, -5);
-                var tcCC = _destructionCircleInConstruction.AddComponent<CircleComponent>();
-                tcCC.Init(_lineWidth, _radius, _color, ToConstructMat, true, AnimationDuration, new float[] { degree, degree });
-                tcCC.StepsIndex = _circleBase.GetComponent<CircleComponent>().StepsIndex;
-            }
 
+            }
         }
 
         public void Destroy()
--- Assets/Scripts/CircleComponent.cs
+++ Assets/Scripts/CircleComponent.cs
@@ -1,140 +1,146 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class CircleComponent : MonoBehaviour
+namespace Assets.Scripts
 {
-    private const float _small = 0.000001f;
-
-    private float _animationDuration;
-
-    private List<float> _lineWidth;
-    private List<float> _radius;
-    private Color _color;
-    private Material _circleMat;
-
-    private LineRenderer _line;
-    private bool _reduce = false;
-    private float _reduceCounter = 0;
-    private float _currentRadius;
-    private float _currentWidth;
-    private bool _isToConstruct;
-    private float[] _toConstructDegrees;
-    private int _stepsNumber;
-    public int StepsIndex = 0;
-
-    public void Init(List<float> lineWidth, List<float> radius, Color color, Material circleMat, bool isToConstruct, float animationDuration, float[] toConstructDegrees)
+    public class CircleComponent : MonoBehaviour
     {
-        _lineWidth = lineWidth;
-        _radius = radius;
-        _color = color;
-        _animationDuration = animationDuration;
-        _isToConstruct = isToConstruct;
-        _circleMat = circleMat;
-        _toConstructDegrees = toConstructDegrees;
-        _line = gameObject.AddComponent<LineRenderer>();
-        _stepsNumber = lineWidth.Count;
 
-        _currentRadius = _radius[0];
-        _currentWidth = _lineWidth[0];
+        private const float _small = 0.000001f;
 
-        DrawCircle(_currentRadius, _currentWidth);
-    }
+        private float _animationDuration;
 
+        private List<float> _lineWidth;
+        private List<float> _radius;
+        private Color _color;
+        private Material _circleMat;
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Asked to reduce
-        if (_reduce)
+        private LineRenderer _line;
+        private bool _reduce = false;
+        private float _reduceCounter = 0;
+        private float _currentRadius;
+        private float _currentWidth;
+        private bool _isToConstruct;
+        private float[] _toConstructDegrees;
+        private int _stepsNumber;
+        public int StepsIndex = 0;
+
+        public void Init(List<float> lineWidth, List<float> radius, Color color, Material circleMat, bool isToConstruct, float animationDuration, float[] toConstructDegrees)
         {
-            _reduceCounter += Time.deltaTime / _animationDuration;
-            // if done reducing
-            if (_reduceCounter >= 1)
-            {
-                var newWidth = StepsIndex < _stepsNumber ? _lineWidth[StepsIndex] : _small;
-                var newRadius = StepsIndex < _stepsNumber ? _radius[StepsIndex] : _small;
-                DrawCircle(newRadius, newWidth);
+            _lineWidth = lineWidth;
+            _radius = radius;
+            _color = color;
+            _animationDuration = animationDuration;
+            _isToConstruct = isToConstruct;
+            _circleMat = circleMat;
+            _toConstructDegrees = toConstructDegrees;
+            _line = gameObject.AddComponent<LineRenderer>();
+            _stepsNumber = lineWidth.Count;
 
-                _reduce = false;
-                _reduceCounter = 0;
-                if (_lineWidth.Count == 0 && _radius.Count == 0)
-                {
-                    // trigger destroy level
-                }
-            } else
+            _currentRadius = _radius[0];
+            _currentWidth = _lineWidth[0];
+
+            DrawCircle(_currentRadius, _currentWidth);
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Asked to reduce
+            if (_reduce)
             {
-                // reducing
-                var newWidth = Mathf.Lerp(_currentWidth, StepsIndex < _stepsNumber ? _lineWidth[StepsIndex] : _small, _reduceCounter);
-                var newRadius = Mathf.Lerp(_currentRadius, StepsIndex < _stepsNumber ? _radius[StepsIndex] : _small, _reduceCounter);
-                DrawCircle(newRadius, newWidth);
+                _reduceCounter += Time.deltaTime / _animationDuration;
+                // if done reducing
+                if (_reduceCounter >= 1)
+                {
+                    var newWidth = StepsIndex < _stepsNumber ? _lineWidth[StepsIndex] : _small;
+                    var newRadius = StepsIndex < _stepsNumber ? _radius[StepsIndex] : _small;
+                    DrawCircle(newRadius, newWidth);
+
+                    _reduce = false;
+                    _reduceCounter = 0;
+                    if (_lineWidth.Count == 0 && _radius.Count == 0)
+                    {
+                        // trigger destroy level
+                    }
+                }
+                else
+                {
+                    // reducing
+                    var newWidth = Mathf.Lerp(_currentWidth, StepsIndex < _stepsNumber ? _lineWidth[StepsIndex] : _small, _reduceCounter);
+                    var newRadius = Mathf.Lerp(_currentRadius, StepsIndex < _stepsNumber ? _radius[StepsIndex] : _small, _reduceCounter);
+                    DrawCircle(newRadius, newWidth);
+                }
             }
         }
-    }
 
-    public void Reduce()
-    {
-        _reduce = true;
-        _currentRadius = _radius[StepsIndex];
-        _currentWidth = _lineWidth[StepsIndex];
-        StepsIndex++;
-    }
-
-    private void DrawCircle(float radius, float lineWidth)
-    {
-        var segments = 600;
-        _line.useWorldSpace = false;
-        _line.startWidth = lineWidth;
-        _line.endWidth = lineWidth;
-        _line.material = _circleMat;
-        _line.startColor = _color;
-        _line.endColor = _color;
-
-        var pointCount = segments + 1; // add extra point to make startpoint and endpoint the same to close the circle
-        var points = new List<Vector3>();
-
-        for (int i = 0; i < pointCount; i++)
+        public void Reduce()
         {
-            var deg = (i * 360f / segments);
-            var shouldDraw = false;
-            if (_isToConstruct)
+            _reduce = true;
+            _currentRadius = _radius[StepsIndex];
+            _currentWidth = _lineWidth[StepsIndex];
+            StepsIndex++;
+        }
+
+        private void DrawCircle(float radius, float lineWidth)
+        {
+            var segments = 600;
+            _line.useWorldSpace = false;
+            _line.startWidth = lineWidth;
+            _line.endWidth = lineWidth;
+            _line.material = _circleMat;
+            _line.startColor = _color;
+            _line.endColor = _color;
+
+            var pointCount = segments + 1; // add extra point to make startpoint and endpoint the same to close the circle
+            var points = new List<Vector3>();
+
+            for (int i = 0; i < pointCount; i++)
             {
-                if (deg >= _toConstructDegrees[0] && deg <= _toConstructDegrees[1])
+                var deg = (i * 360f / segments);
+                var shouldDraw = false;
+                if (_isToConstruct)
+                {
+                    if (deg >= _toConstructDegrees[0] && deg <= _toConstructDegrees[1])
+                    {
+                        shouldDraw = true;
+                    }
+                }
+                else
                 {
                     shouldDraw = true;
                 }
-            } else
-            {
-                shouldDraw = true;
+                if (shouldDraw)
+                {
+                    var rad = Mathf.Deg2Rad * (i * 360f / segments);
+                    points.Add(new Vector3(Mathf.Sin(rad) * radius, Mathf.Cos(rad) * radius, 0));
+                }
             }
-            if (shouldDraw)
-            {
-                var rad = Mathf.Deg2Rad * (i * 360f / segments);
-                points.Add(new Vector3(Mathf.Sin(rad) * radius, Mathf.Cos(rad) * radius, 0));
-            }
+            var arr = points.ToArray();
+            _line.positionCount = arr.Length;
+            _line.SetPositions(arr);
         }
-        var arr = points.ToArray();
-        _line.positionCount = arr.Length;
-        _line.SetPositions(arr);
-    }
 
-    public float[] GetDegrees()
-    {
-        return _toConstructDegrees;
-    }
+        public float[] GetDegrees()
+        {
+            return _toConstructDegrees;
+        }
 
-    public void UpdateDegreeIn(float a)
-    {
-        _toConstructDegrees[0] = a;
-    }
+        public void UpdateDegreeIn(float a)
+        {
+            _toConstructDegrees[0] = a;
+        }
 
-    public void UpdateDegreeOut(float a)
-    {
-        _toConstructDegrees[1] = a;
-    }
+        public void UpdateDegreeOut(float a)
+        {
+            _toConstructDegrees[1] = a;
+        }
 
-    public void RedrawCircle()
-    {
-        DrawCircle(_radius[StepsIndex], _lineWidth[StepsIndex]);
+        public void RedrawCircle()
+        {
+            DrawCircle(_radius[StepsIndex], _lineWidth[StepsIndex]);
+        }
+
     }
 }
